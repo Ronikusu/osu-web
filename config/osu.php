8d8743b54909967a57983a4891d6673d6e1b95c8
nanaya <me@nanaya.pro>
2022-12-05 17:43:10 +0900

    Sort name

--- config/osu.php
+++ config/osu.php
@@ -164,8 +164,8 @@ return [
         'storage' => env('SCORE_REPLAYS_STORAGE', 'local'),
     ],
     'scores' => [
-        'es_enable_legacy_filter' => get_bool(env('SCORES_ES_ENABLE_LEGACY_FILTER')) ?? true,
         'es_cache_duration' => 60 * (get_float(env('SCORES_ES_CACHE_DURATION')) ?? 0.5), // in minutes, converted to seconds
+        'es_enable_legacy_filter' => get_bool(env('SCORES_ES_ENABLE_LEGACY_FILTER')) ?? true,
         'rank_cache' => [
             'local_server' => get_bool(env('SCORES_RANK_CACHE_LOCAL_SERVER')) ?? false,
             'min_users' => get_int(env('SCORES_RANK_CACHE_MIN_USERS')) ?? 35000,
